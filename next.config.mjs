5ead3f3405cb827ae40dc04f1e267b1681d4d802
jiyeon <jiyeonyun43@gmail.com>
2024-04-03 22:59:57 +0900

    [feat] 초기설정 -git ignore 설정 -next.config 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+    swcMinify: true, // 코드 경량화
+    compiler: {
+        styledComponents: true,
+    },
+    images: {
+        formats: ["image/avif", "image/webp"],
+        minimumCacheTTL: 31536000,
+        imageSizes: [32, 64, 96, 128, 256],
+        deviceSizes: [750, 1080, 1200, 1920, 2048],
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
 
 export default nextConfig;
