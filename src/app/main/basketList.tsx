f9fb5f1f38670012717748916ce177e4c663ba34
jiyeon <jiyeonyun43@gmail.com>
2024-04-16 23:28:43 +0900

    [feat] 열고닫기 수정

--- src/app/main/basketList.tsx
+++ src/app/main/basketList.tsx
@@ -5,37 +5,42 @@ import Image from "next/image";
 const BasketList = ({ basketInfo, setCenter, setZoom }: any) => {
     const [isOpen, setIsOpen] = useState(false);
     return (
-        <div className={isOpen ? styles.basketListOpen : styles.basketListClose}>
-            <span
-                onClick={() => {
-                    setIsOpen(!isOpen);
-                }}
-                className={styles.headerButton}
-            >
-                Open
-            </span>
-            <div className={styles.basketinnerWrapper}>
-                {basketInfo.map((item: any, i: number) => {
-                    return (
-                        <div
-                            onClick={() => {
-                                setCenter([Number(item.y), Number(item.x)]);
-                                setZoom(4);
-                            }}
-                            className={styles.basketCard}
-                        >
-                            <img src={item.imgurl} alt={""} />
-                            <h3>
-                                {i + 1}.{item.placenm}
-                            </h3>
-                            <p>{item.payatnm}</p>
-                            <p>
-                                {item.v_min}~{item.v_max}
-                            </p>
-                            <p>{item.telno}</p>
-                        </div>
-                    );
-                })}
+        <div style={{ display: "flex" }}>
+            <div className={isOpen ? styles.basketOuterWrapperOpen : styles.basketOuterWrapperClose}>
+                <span
+                    onClick={() => {
+                        setIsOpen(!isOpen);
+                    }}
+                    className={styles.headerButton}
+                >
+                    {isOpen ? "➡️" : "⬅️"}
+                </span>
+            </div>
+
+            <div className={isOpen ? styles.basketListOpen : styles.basketListClose}>
+                <div className={styles.basketinnerWrapper}>
+                    {basketInfo.map((item: any, i: number) => {
+                        return (
+                            <div
+                                onClick={() => {
+                                    setCenter([Number(item.y), Number(item.x)]);
+                                    setZoom(4);
+                                }}
+                                className={styles.basketCard}
+                            >
+                                <img src={item.imgurl} alt={""} />
+                                <h3>
+                                    {i + 1}.{item.placenm}
+                                </h3>
+                                <p>{item.payatnm}</p>
+                                <p>
+                                    {item.v_min}~{item.v_max}
+                                </p>
+                                <p>{item.telno}</p>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
